c9a1381defe0b89290ed05a538f351a972e7c5cb
Jon Como <jon@underground.net>
2013-09-01 17:40:22 -0700

    Initial Commit

---
+++ RatRace/RRViewController.h
@@ -0,0 +1,13 @@
+//
+//  RRViewController.h
+//  RatRace
+//
+//  Created by Jon Como on 9/1/13.
+//  Copyright (c) 2013 Jon Como. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RRViewController : UIViewController
+
+@end
---
+++ RatRace/RRViewController.m
@@ -0,0 +1,29 @@
+//
+//  RRViewController.m
+//  RatRace
+//
+//  Created by Jon Como on 9/1/13.
+//  Copyright (c) 2013 Jon Como. All rights reserved.
+//
+
+#import "RRViewController.h"
+
+@interface RRViewController ()
+
+@end
+
+@implementation RRViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
