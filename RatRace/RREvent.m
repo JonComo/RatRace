6d35f444f5a3ede84521bcc751c227573793fcc7
Jon Como <jon@underground.net>
2013-09-04 16:07:27 -0700

    working on events

---
+++ RatRace/RREvent.m
@@ -0,0 +1,67 @@
+//
+//  RREvent.m
+//  RatRace
+//
+//  Created by Jon Como on 9/4/13.
+//  Copyright (c) 2013 Jon Como. All rights reserved.
+//
+
+#import "RREvent.h"
+
+@implementation RREvent
+{
+    int daysProgressed;
+}
+
++(RREvent *)eventWithInitialBlock:(InitialBlock)initBlock numberOfDays:(int)duration endingBlock:(EndingBlock)endBlock
+{
+    RREvent *newEvent = [[self alloc] init];
+    
+    newEvent.initBlock = initBlock;
+    newEvent.endBlock = endBlock;
+    
+    newEvent.duration = duration;
+    
+    return newEvent;
+}
+
+-(id)init
+{
+    if (self = [super init]) {
+        //init
+        daysProgressed = 0;
+    }
+    
+    return self;
+}
+
+-(void)runStart
+{
+    if (self.initBlock)
+        self.initBlock();
+}
+
+-(void)runEnd
+{
+    if (daysProgressed >= self.duration){
+        if (self.endBlock)
+            self.endBlock();
+    }
+}
+
+-(void)progressDay
+{
+    if (daysProgressed == 0)
+    {
+        //first day, run it
+        [self runStart];
+    }
+    
+    daysProgressed ++;
+    
+    if (daysProgressed >= self.duration){
+        [self runEnd];
+    }
+}
+
+@end
