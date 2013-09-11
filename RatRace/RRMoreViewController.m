5a9ad588d6c313d8c7ec6a85909af91247c7446e
zoosjuice <dkdejesus@gmail.com>
2013-09-10 17:10:53 -0700

    how to added

---
+++ RatRace/RRMoreViewController.m
@@ -0,0 +1,30 @@
+//
+//  RRMoreViewController.m
+//  RatRace
+//
+//  Created by David de Jesus on 9/10/13.
+//  Copyright (c) 2013 Jon Como. All rights reserved.
+//
+
+#import "RRMoreViewController.h"
+
+@interface RRMoreViewController ()
+
+@end
+
+@implementation RRMoreViewController
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+- (IBAction)done:(id)sender {
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
